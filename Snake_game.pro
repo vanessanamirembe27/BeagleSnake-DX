75eb371dc69a12f62d6c61b97e68dd4f48a241b3
jasonpliszak <jpliszak@bu.edu>
2025-11-29 00:06:50 -0500

    Implemented core snakegame logic + base view

---
+++ Snake_game.pro
@@ -0,0 +1,26 @@
+# Specify the Qt modules your application uses.
+# You need 'widgets' for QApplication, QGraphicsView, etc.
+# 'gui' is needed for QColor, QMouseEvent, etc.
+QT += core gui widgets
+
+# Set the name of the final executable file.
+TARGET = BeagleSnake-DX
+
+# Specify that this is an application project.
+TEMPLATE = app
+
+# Add all your source files (.cpp).
+SOURCES += \
+    src/main.cpp \
+    src/snakegame.cpp \
+    src/gameview.cpp
+
+# Add all your header files (.h).
+HEADERS += \
+    src/snakegame.h \
+    src/gameview.h \
+    src/config.h
+
+# It's good practice to specify the C++ standard.
+# Your use of 'constexpr' in config.h requires at least C++11.
+CONFIG += c++11
---
+++ src/snakegame.h
@@ -0,0 +1,57 @@
+#ifndef SNAKEGAME_H
+#define SNAKEGAME_H
+
+#include <QObject>
+#include <QList>
+#include <QPoint>
+
+class SnakeGame : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SnakeGame(int boardWidth, int boardHeight, QObject *parent = nullptr);
+
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    enum GameState {
+        Playing,
+        Paused,
+        GameOver
+    };
+
+    void initGame();
+    void tick();
+    void setDirection(Direction newDirection);
+    void togglePause(); 
+
+    const QList<QPoint>& getSnake() const;
+    const QPoint& getFood() const;
+    int getScore() const;
+    GameState getGameState() const;
+
+signals:
+    void gameUpdated();
+    void scoreChanged(int score);
+    void gameOver();
+
+private:
+    void moveSnake();
+    void checkCollision();
+    void placeFood();
+
+    QList<QPoint> m_snake;
+    QPoint m_food;
+    Direction m_direction;
+    GameState m_gameState;
+    int m_score;
+
+    const int m_boardWidth;
+    const int m_boardHeight;
+};
+
+#endif // SNAKEGAME_H
