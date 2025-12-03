c8d268dd34bd6cac1153da2a88d19f80a8367506
vanessanamirembe27 <147095737+vanessanamirembe27@users.noreply.github.com>
2025-12-03 16:43:56 -0500

    Replace GameView with GameWindow for fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <QApplication> 
-//#include "gamewindow.h" add back once implemented pause screen/ title screen 
+#include "gamewindow.h" add back once implemented pause screen/ title screen 
 #include "gameview.h"
 
 int main(int argc, char *argv[])
 { 
     QApplication a(argc, argv); 
 
-    GameView view; //starts game instance
-
-    view.showFullScreen(); //want full screen on LCD Cape 
+    GameWindow window;
+    window.showFullScreen();
 
     return a.exec(); 
 }
