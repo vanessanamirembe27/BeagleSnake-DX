a6a0f95434502ee43f7bfdf6427be6309553eeb4
vanessanamirembe27 <147095737+vanessanamirembe27@users.noreply.github.com>
2025-12-05 13:37:46 -0500

    Enhance GameWindow with menu and instruction screens
    Added menu navigation and instruction screens to GameWindow.

--- src/gamewindow.h
+++ src/gamewindow.h
@@ -5,9 +5,11 @@
 #include <QPushButton>
 #include <QVBox Layout>
 #include <QHBox Layout>
+#include <QStackWidget>
 
 class GameView;
 class SnakeGame;
+class ToneBuzzer;
 class GameWindow : public QWidget
 {
   Q_OBJECT
@@ -15,22 +17,44 @@ public:
     explicit GameWindow(QWidget *parent = nullptr);
 
 private slots:
+    //Menu Navigation
+    void startGame();
+    void showInstructions();
+    void exitGame();
+    //In game UI
     void updateScore(int newScore);
     void handleGameOver();
     void pauseGame();
     void restartGame();
 
 private:
-    void createUI();
+    void createMenuScreen();
+    void createInstructionsScreen();
+    void createGameScreen();
     void connectSignals();
 
+   //Screens
+    QStackedWidget *m_stack;
+    QWidget *m_menuScreen;
+    QWidget *m_instructionScreen;
+    QWidget *m_gameScreen;
+
+   // Game components
     GameView *m_view;
     SnakeGame *m_game;
+    ToneBuzzer *m_buzzer;
+
+   // In-game UI elements
     Qlabel *m_scorelabel;
     QPushButton *m_pauseButton;
     QPushButton *m_restartButton;
+
+   // Layouts
     QVBoxLayout *m_mainLayout;
     QHBoxLayout *m_topBarLayout;
+
+   //Game State
     bool m_isPaused = false;
 };
-#endif //
+#endif // GAMEWINDOW_H
+
